bc07d2da2bb11f2c98487b7abf95367352170a0d
Vi <vanessareigosa@gmail.com>
2019-10-02 16:56:22 -0700

    wow holy moly this is working! task index is up and running, you can add to it, working on making an edit view page

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all  
+  end 
+
+  def show
+    task_id = params[:id].to_i
+
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_Found
+      return
+    end 
+  end 
+
+  def new
+    @task = Task.new
+  end 
+
+  def create
+    @task = Task.new( name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date] )
+    
+    if @task.save
+      redirect_to task_path(@task.id)
+    else 
+      render new_task_path
+    end 
+  end 
+
+  def edit
+    @task = Task.find_by(id: params[:id])  
+  end 
+
+  def update
+    @task = Task.find_by(id: params[:id], name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+
+    if @task.save
+      redirect_to task_path(@task.id)
+    else 
+      render new_task_path
+    end 
+  end 
+
+end
